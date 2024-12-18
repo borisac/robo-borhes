a79565f73b7ff3d8feff9cb3d01e956d5b65043f
Borisa <borisa.cajic@gmail.com>
2024-12-18 19:07:32 +0100

    aditional router-iew adding child routes, updating router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartsInfo from '../parts/PartsInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -16,6 +21,33 @@ export default createRouter({
       path: '/build',
       name: 'Build',
       component: RobotBuilder,
+    },
+    {
+      path: '/parts/browse/',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          name: 'BrowseHeads',
+          path: 'heads',
+          component: RobotHeads,
+        },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms,
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torso',
+          component: RobotTorsos,
+        },
+        {
+          name: 'BrowseBases',
+          path: 'base',
+          component: RobotBases,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:id',
