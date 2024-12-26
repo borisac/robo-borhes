79182efde5d2c0c53064a71d4f828263bf163ed7
Borisa <borisa.cajic@gmail.com>
2024-12-26 23:36:23 +0100

    pisanje direktiva na drugi nacin

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-param-reassign */
 
-function applyStyle(element, binding) {
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
 
-export default {
-  beforeMount(element, binding) {
-    applyStyle(element, binding);
-  },
-  updated: (element, binding) => {
-    applyStyle(element, binding);
-  },
-};
+// function applyStyle(element, binding) {
+//   Object.keys(binding.value).forEach((position) => {
+//     element.style[position] = binding.value[position];
+//   });
+//   element.style.position = 'absolute';
+// }
+
+// export default {
+//   beforeMount(element, binding) {
+//     applyStyle(element, binding);
+//   },
+//   updated: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+// };
